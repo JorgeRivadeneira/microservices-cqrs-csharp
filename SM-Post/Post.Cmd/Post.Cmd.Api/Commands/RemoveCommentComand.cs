148bb934fa0b942d398d31472e02919edbbd5369
Jorge Rivadeneira Cevallos <jorge.rivadeneira.cevallos@gmail.com>
2024-08-21 15:36:17 -0500

    First Commit

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/RemoveCommentComand.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class RemoveCommentComand: BaseCommand
+    {
+        public Guid CommentId { get; set; }
+        public string Username { get; set; }
+    }
+}
